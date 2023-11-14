58fff01536584e53d6c8d981f536b01863f9434a
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-13 23:36:05 -0800

    book model and data imported to index route file

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 //PART OF EXPRESS SETUP
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Book = require("../models").Book;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async(req, res)=>{
+  const books = await Book.findAll();
+  console.log(books);
+  res.json(books);
 });
 
 module.exports = router;
