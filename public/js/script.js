9ae2aab1cd7bbb5f2bca986f549b56a82026b816
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-16 12:13:22 -0800

    started writing pagination function

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,10 @@
 const booksPerPage = 10;
 const bookList = document.querySelector('.book-entries');
 const pageBtns = document.querySelector('.link-list');
+
+//SHOW PAGE FUNCTION
+
+function showPage(bookEntries, page){
+    const startIndex = (page*booksPerPage) - booksPerPage;
+    const endIndex = (page*booksPerPage);
+}
